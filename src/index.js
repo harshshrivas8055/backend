8ab27addef383d081fe3bcbec8e71ff9002fb4ca
harshshrivas8055 <harshkumarshrivas128@gmail.com>
2025-02-08 15:30:18 +0530

    learn utils code and  express

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(() => {
+   app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is runnning at port : ${process.env.PORT}`);
+   }) 
+   app.on("error", (error) => {
+     console.log("Server is not runnning ERRR:", error);
+     throw error;
+   })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
